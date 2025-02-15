eaf9907873ad21588633fb1f0cdb331580d60173
Colt Ossoff <coltossoff@gmail.com>
2025-02-15 16:01:25 -0600

    basic starter api

---
+++ src/app/models/joke.ts
@@ -0,0 +1,6 @@
+export interface Joke {
+    jokeId: number | undefined;
+    title: string;
+    content: string;
+    author: string;
+}
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,16 @@ import {
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import fs from 'node:fs';
+import { Joke } from './app/models/joke';
+
+var jokes: Joke[];
+try {
+  const data = fs.readFileSync('./jokes.json', 'utf8');
+  jokes = JSON.parse(data);
+} catch (err) {
+  console.error(err);
+}
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -49,6 +59,29 @@ app.use('/**', (req, res, next) => {
     .catch(next);
 });
 
+
+app.get('/joke', (req, res, next) => {
+  try {
+    const rand = Math.floor(Math.random() * (jokes.length));
+    let joke: Joke = jokes[rand];
+    if (joke == undefined) throw 'Out of Bounds';
+    res.json(joke);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+app.post('/joke', (req, res) => {
+  let entry: Joke = req.body;
+  try {
+    jokes.push(entry)
+    res.json(entry);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
 /**
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
